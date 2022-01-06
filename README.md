8886d0d8f841345800e2ab057ea053874602ff17
Yevgnen Koh <wherejoystarts@gmail.com>
2022-01-06 22:21:29 +0800

    Simplify the playground

---
+++ README.md
@@ -0,0 +1,58 @@
+<!-- *- mode:markdown;mode:orgtbl;fill-column:99 -* -->
+# rust-playground [![MELPA Stable](https://stable.melpa.org/packages/rust-playground-badge.svg)](https://stable.melpa.org/#/rust-playground) [![MELPA](https://melpa.org/packages/rust-playground-badge.svg)](https://melpa.org/#/rust-playground)
+
+GNU/Emacs mode that setup local playground for code snippets in Rust language.  This is a port of
+[go-playground](https://github.com/grafov/go-playground) adapted for Rust environment.
+
+## Install
+
+Install `rust-playground` from MELPA:
+
+    M-x package-install RET rust-playground
+
+The mirror repository of the project: https://notabug.org/grafov/rust-playground.
+I really not think that this small project need the mirror but I just would like advertise a nice
+git hosting for FOSS. Try [notabug.org](https://notabug.org) for your projects! :)
+
+Example screen after creation of a new snippet:
+
+![screenshot](rust-playground-after-start.png)
+
+## Usage
+
+### Quick start
+
+1. From any mode run `M-x rust-playground` for start a new playground buffer filled with basic
+   template for the package with main function (see the picture below).
+1. Add your code then press `Ctl-Return` (it bound to `rust-playground-exec` command). It will save,
+   compile and exec the snippet code.
+1. When you played enough with this snippet just run `M-x rust-playground-rm`. It will remove the
+   current snippet with its directory and all files.
+
+### List of interactive functions
+
+<!---
+#+ORGTBL: SEND keys orgtbl-to-gfm
+| Function name              | Description                                                          |
+|----------------------------+----------------------------------------------------------------------|
+| `rust-playground`          | Create a new playground buffer with basic template for the package.  |
+| `rust-playground-download` | [WIP] Download the snippet from the URL at play.rust-lang.org.       |
+| `rust-playground-exec`     | Save, compile and run the code of the snippet.                       |
+| `rust-playground-upload`   | [WIP] Upload the buffer to play.golang.org and return the short URL. |
+| `rust-playground-rm`       | Remove the snippet with its directory with all files.                |
+-->
+<!--- BEGIN RECEIVE ORGTBL keys -->
+| Function name | Description |
+|---|---|
+| `rust-playground` | Create a new playground buffer with basic template for the package. |
+| `rust-playground-download` | [WIP] Download the snippet from the URL at play.rust-lang.org. |
+| `rust-playground-exec` | Save, compile and run the code of the snippet. |
+| `rust-playground-upload` | [WIP] Upload the buffer to play.golang.org and return the short URL. |
+| `rust-playground-rm` | Remove the snippet with its directory with all files. |
+<!--- END RECEIVE ORGTBL keys -->
+
+## Project status
+
+The plugin is usable. I've stopped learning Rust yet so this Emacs plugin not very interesting for
+me. But I'll continue to maintain it and backport features from `go-playground`. Patches are
+welcomed!
---
+++ rust-playground.el
@@ -0,0 +1,106 @@
+;;; rust-playground.el --- Local Rust playground for short code snippets.
+
+;; Copyright (C) 2016-2017  Alexander I.Grafov (axel)
+
+;; Author: Alexander I.Grafov <grafov@gmail.com> + all the contributors (see git log)
+;; URL: https://github.com/grafov/rust-playground
+;; Version: 0.2.1
+;; Keywords: tools, rust
+;; Package-Requires: ((emacs "24.3"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Local playground for the Rust programs similar to play.rust-lang.org.
+;; `M-x rust-playground` and type you rust code then make&run it with `C-c C-c`.
+;; Toggle between Cargo.toml and main.rs with `C-c b`
+;; Delete the current playground and close all buffers with `C-c k`
+
+;; Playground requires preconfigured environment for Rust language.
+
+;; It is port of github.com/grafov/go-playground for Go language.
+
+;;; Code:
+
+(require 'compile)
+
+(defgroup rust-playground nil
+  "Options specific to Rust Playground.")
+
+(defcustom rust-playground-basedir (locate-user-emacs-file "rust-playground")
+  "Base directory for playground snippets."
+  :type 'file
+  :group 'rust-playground)
+
+(defcustom rust-playground-cargo-toml-template
+  (format "[package]
+name = \"project\"
+version = \"0.1.0\"
+authors = [\"%s <%s>\"]
+edition = \"2021\"
+
+[dependencies]
+" (or user-full-name "Rust Example") (or user-mail-address "rust@example.com"))
+  "When creating a new playground, this will be used as the Cargo.toml file")
+
+(defcustom rust-playground-main-rs-template
+  "fn main() {\n    \n}"
+  "When creating a new playground, this will be used as the body of the main.rs file")
+
+(defcustom rust-playground-dir-format
+  "%Y-%m-%d_%H-%M-%S"
+  "Directory name pattern for the playground project")
+
+(defun rust-playground-dir-name ()
+  (file-name-as-directory
+   (concat (file-name-as-directory rust-playground-basedir)
+           (format-time-string rust-playground-dir-format))))
+
+(defun rust-playground-snippet-main-file-name (basedir)
+  "Get the snippet main.rs file from BASEDIR."
+  (concat basedir (file-name-as-directory "src") "main.rs"))
+
+(defun rust-playground-toml-file-name (basedir)
+  "Get the cargo.toml filename from BASEDIR."
+  (concat basedir "Cargo.toml"))
+
+(defun rust-playground-new-template (filename template &optional open)
+  (with-temp-buffer
+    (insert template)
+    (set-visited-file-name filename t)
+    (save-buffer))
+  (if open
+      (find-file filename)))
+
+(defun rust-playground-set-point ()
+  (when (derived-mode-p 'rust-mode)
+    (goto-char (point-max))
+    (re-search-backward "}")
+    (backward-char 1)))
+
+;;;###autoload
+(defun rust-playground ()
+  (interactive)
+  (let* ((basedir (rust-playground-dir-name))
+         (toml-filename (rust-playground-toml-file-name basedir))
+         (main-filename (rust-playground-snippet-main-file-name basedir)))
+    (make-directory (expand-file-name "src" basedir) 'parents)
+    (rust-playground-new-template toml-filename rust-playground-cargo-toml-template)
+    (rust-playground-new-template main-filename rust-playground-main-rs-template 'open)
+    (rust-playground-set-point)))
+
+(provide 'rust-playground)
+
+;;; rust-playground.el ends here
